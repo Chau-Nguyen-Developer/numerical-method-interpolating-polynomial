4c61fad0b0c78bef96a84226b4cba81f781c1d4a
Ngoc Chau Nguyen <ngocchaun1@cpp.edu>
2025-04-23 11:43:20 -0700

    Read two lines of number into two separate vectors.

--- programming4.cpp
+++ programming4.cpp
@@ -6,6 +6,7 @@
 #include <iostream> //do I need it?
 #include <fstream>
 #include <sstream> //string stream -- allows me to treat a string as if it is a stream
+#include <vector>
 
 
 int main()
@@ -13,21 +14,62 @@ int main()
     //Read two lines of numbers in input.txt and store them into two vectors
     //std::ifstream -- standard input file stream. Read data from a file.
     std::ifstream inputFile("input.txt");
-    std::string word;
+    std::string line;
+    std::vector<double> vector1;
+    std::vector<double> vector2;
+
 
     if(inputFile.is_open())
     {
-        while(inputFile >> word)
+        //Read the first line into first vector.
+        //(Use while loop if want to read continuously)
+
+        if(std::getline(inputFile, line)) 
+        //getline function reads an entire line of text from a stream
+        //(in this case, that stream is inputFile)
         {
-            //while input file was still able to read word-by-word
-            std::cout << word << std::endl;
+            std::stringstream ss(line); //treat string like a stream. Able to extract tokens using >>.
+            double number;
+            while(ss >> number)
+            {
+                vector1.push_back(number);
+            }
+
         }
-        
+
+        //Read the second line  //A few thought: maybe create a separate function to read each line
+        if(std::getline(inputFile, line))
+        {
+            std::stringstream ss(line);
+            double number;
+            while(ss >> number)
+            {
+                vector2.push_back(number);
+            }
+        }
+
         inputFile.close(); //closes the file
     }
     else
     {
         std::cout << "File could not be opened!" << std::endl;
+    }
+
+    //Print out vectors to check. 
+    std::cout << "My Vector1: " << std::endl;
+
+    for(const double& num: vector1)
+    {
+        std::cout << num << " ";
+    }
+    
+    std::cout <<std::endl;
+    
+    std::cout << "My Vector2: " << std::endl;
+
+    for(const double& num: vector2)
+    {
+        std::cout << num << " ";
     }
 
     return 0;
